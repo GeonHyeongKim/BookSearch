b1f94be8a2b975506a4694fa7547ad0f577bee89
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-07-05 20:40:30 +0900

    [Chore] 프로젝트 생성

---
+++ BookSearch/BookSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookSearchApp.swift
+//  BookSearch
+//
+//  Created by geonhyeong on 2023/07/05.
+//
+
+import SwiftUI
+
+@main
+struct BookSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
