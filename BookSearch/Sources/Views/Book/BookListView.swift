6b6a3b4b1485517287c5608da8da828726250fa8
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-07-05 23:16:05 +0900

    [Feat] 검색 textFiled view

--- BookSearch/Sources/Views/Book/BookListView.swift
+++ BookSearch/Sources/Views/Book/BookListView.swift
@@ -9,10 +9,18 @@ import SwiftUI
 
 struct BookListView: View {
 	//MARK: Property Wrapper
+	@State private var searchText = ""
 
     var body: some View {
 		VStack {
-			
+			HStack {
+				Image(systemName: "magnifyingglass")
+					.foregroundColor(Color.black)
+					.padding(.leading, 10)
+				
+				TextField("검색어를 입력해주세요", text: $searchText)
+					.modifier(TextFieldClearButton(text: $searchText))
+			} // HStack
 		} // VStack
 		.padding()
 		.navigationTitle("검색 화면")
