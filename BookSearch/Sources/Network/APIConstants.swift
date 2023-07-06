c843a6f92444d1bdceaab273833f01163a72bb7f
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-07-06 13:39:40 +0900

    [Fix] APIConstnats 파일 위치로 인한 문제 해결

--- BookSearch/Sources/Network/APIConstants.swift
+++ BookSearch/Sources/Network/APIConstants.swift
@@ -1,12 +1,8 @@
 //
-//  APIConstants2.swift
+//  APIConstants.swift
 //  BookSearch
 //
 //  Created by geonhyeong on 2023/07/06.
 //
 
 import Foundation
-
-enum APIConstants {
-	static let baseURL = "https://openlibrary.org"
-}
