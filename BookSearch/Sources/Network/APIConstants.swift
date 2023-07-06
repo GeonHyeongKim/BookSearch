13c0309a444a38f861559185a6679e79cee4fb1e
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-07-06 16:21:09 +0900

    [Feat] APIConstants base url 주소 변경 및 static 변수 추가

--- BookSearch/Sources/Network/APIConstants.swift
+++ BookSearch/Sources/Network/APIConstants.swift
@@ -7,6 +7,12 @@
 
 import Foundation
 
-enum APIConstants {
-	static let baseURL = "https://openlibrary.org"
+struct APIConstants {
+	static let baseURL = "https://openlibrary.org/"
+	static let coverBaseURL = "https://covers.openlibrary.org/"
+
+	struct Path {
+		static let searchPath = baseURL + "search.json?q="
+		static let imageURLPath = coverBaseURL + "b/id/"
+	}
 }
