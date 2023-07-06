44e46520b55d6c0727c6a52e65e02437a6e3b4a4
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-07-06 12:35:17 +0900

    [Feat] APIConstants baseURL 설정

---
+++ BookSearch/Sources/Network/APIConstants.swift
@@ -0,0 +1,12 @@
+//
+//  APIConstants2.swift
+//  BookSearch
+//
+//  Created by geonhyeong on 2023/07/06.
+//
+
+import Foundation
+
+enum APIConstants {
+	static let baseURL = "https://openlibrary.org"
+}
