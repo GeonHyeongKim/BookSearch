7d6138bbd15c672b8dca8dc271dc94551397e597
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-07-07 15:45:09 +0900

    [Style] 함수 설명 주석 추가

--- BookSearch/Sources/Network/ImageDownloader.swift
+++ BookSearch/Sources/Network/ImageDownloader.swift
@@ -22,6 +22,11 @@ actor ImageDownloader {
 		case L
 	}
 	
+	/// URL 을 기반으로 Cache 처리 및 UIImage 로 반환하는 메서드.
+	/// - Parameters:
+	///   - urlPath: 다운받을 cover_i  값.
+	///   - size: Image의 크기
+	/// - Returns: Cache에 저장되어 있는 UIImage를 반환. 변환되지 않는 경우 에러 반환.
 	func image(from urlPath: Int?, size: Size = .M) async throws -> UIImage? {
 		guard let urlPath = urlPath else {
 			throw ImageDownloadError.invalidURLString
@@ -50,6 +55,11 @@ actor ImageDownloader {
 		return image
 	}
 
+	/// 나이 더하기
+	/// - Parameters:
+	///   - a: a의 나이
+	///   - b: b의 나이
+	/// - Returns: 둘 나이의 합
 	private func downloadImage(from url: URL) async throws -> UIImage {
 		let imageFetchProvider = ImageFetchProvider.shared
 		return try await imageFetchProvider.fetchImage(with: url)
